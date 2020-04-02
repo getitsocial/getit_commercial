2769f637912135386f0040c914fcde50f7e1b053
Tayfun Gülcan <tayfunmobile@gmail.com>
2020-04-02 21:50:26 +0200

    Feat: add wysiwyg editor

--- components/elements/article/newForm.vue
+++ components/elements/article/newForm.vue
@@ -60,20 +60,13 @@
       <!-- articleName INPUT -->
       <div class="form-content my-3" :class="{ error: errors[0] }">
         <label class="form-label w-full" for="articleDescription">
-          <span class="text-info">Artikelbeschreibung</span>
           <client-only>
-            <editor-content
-              id="articleDescription"
-              class="form-input"
-              name="articleDescription"
-              :editor="editor"
-            />
+            <wysiwyg @content="(data) => (article.description = data)" />
           </client-only>
           <span class="error-message">{{ errors[0] }}</span>
         </label>
       </div>
     </ValidationProvider>
-    <!-- <pre>{{ article }}</pre> -->
     <bottom-area>
       <button class="primary">
         Artikel anlegen
@@ -84,11 +77,10 @@
 
 <script>
 import { ValidationProvider } from 'vee-validate'
-import { Editor, EditorContent } from 'tiptap'
-import { Bold, Italic, Link, HardBreak, Heading } from 'tiptap-extensions'
 
 import bottomArea from '~/components/layout/bottomarea'
 import imageUpload from '~/components/elements/utils/imageUpload'
+import wysiwyg from '~/components/elements/utils/wysiwyg'
 
 export default {
   name: 'NewArticleForm',
@@ -96,30 +88,14 @@ export default {
     ValidationProvider,
     bottomArea,
     imageUpload,
-    EditorContent
+    wysiwyg
   },
   data: () => ({
     article: {
       image: null,
       description: null
-    },
-    editor: null
+    }
   }),
-  mounted() {
-    this.editor = new Editor({
-      content: '<p>Artikelbeschreibung hier...</p>',
-      extensions: [
-        new Bold(),
-        new Italic(),
-        new Link(),
-        new HardBreak(),
-        new Heading()
-      ]
-    })
-  },
-  beforeDestroy() {
-    this.editor.destroy()
-  },
   methods: {
     setImage(img) {
       this.article.image = img
---
+++ components/elements/utils/wysiwyg.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="editor">
+    <client-only>
+      <editor-menu-bar v-slot="{ commands, isActive }" :editor="editor">
+        <div class="menubar-wrapper">
+          <ul class="menubar">
+            <li>
+              <button
+                :class="{ active: isActive.bold() }"
+                @click="commands.bold"
+              >
+                <b>B</b>
+              </button>
+            </li>
+
+            <li>
+              <button
+                :class="{ active: isActive.italic() }"
+                @click="commands.italic"
+              >
+                <i>I</i>
+              </button>
+            </li>
+
+            <li>
+              <button
+                :class="{ active: isActive.underline() }"
+                @click="commands.underline"
+              >
+                <u>U</u>
+              </button>
+            </li>
+
+            <li>
+              <button
+                :class="{ active: isActive.heading({ level: 1 }) }"
+                @click="commands.heading({ level: 1 })"
+              >
+                H1
+              </button>
+            </li>
+
+            <li>
+              <button
+                :class="{ active: isActive.heading({ level: 2 }) }"
+                @click="commands.heading({ level: 2 })"
+              >
+                H2
+              </button>
+            </li>
+
+            <li>
+              <button
+                :class="{ active: isActive.heading({ level: 3 }) }"
+                @click="commands.heading({ level: 3 })"
+              >
+                H3
+              </button>
+            </li>
+          </ul>
+        </div>
+      </editor-menu-bar>
+
+      <editor-content class="editor-content" :editor="editor" />
+    </client-only>
+  </div>
+</template>
+
+<script>
+import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
+import { Heading, Bold, Italic, Link, Underline } from 'tiptap-extensions'
+export default {
+  components: {
+    EditorContent,
+    EditorMenuBar
+  },
+  data: () => ({
+    editor: null
+  }),
+  mounted() {
+    this.editor = new Editor({
+      extensions: [
+        new Heading({ levels: [1, 2, 3] }),
+        new Link(),
+        new Bold(),
+        new Italic(),
+        new Underline()
+      ],
+      content: `
+          <h3>
+           Artikelbeschreibung
+          </h3>
+          <p>
+            Beschreibe dein Artikel...
+          </p>
+        `,
+      onUpdate: ({ getHTML }) => {
+        this.$emit('content', getHTML())
+      }
+    })
+  },
+  beforeDestroy() {
+    if (!this.editor) return
+    this.editor.destroy()
+  }
+}
+</script>
+<style lang="scss">
+.menubar {
+  &-wrapper {
+    @apply my-1;
+  }
+  @apply flex flex-wrap justify-center;
+  &li {
+    @apply flex-auto;
+  }
+}
+
+.active {
+  @apply bg-grey;
+}
+
+.editor {
+  &-content {
+    @apply mt-3 text-base;
+  }
+}
+</style>
