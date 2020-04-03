b478bb759b87ce57180563ebe9e213dfad9218a1
Tayfun Gülcan <tayfunmobile@gmail.com>
2020-04-03 10:34:30 +0200

    change name

--- components/elements/desktopNav.vue
+++ components/elements/desktopNav.vue
@@ -5,7 +5,7 @@
       <div class="hidden sm:block">
         <nuxt-link to="/" class="no-underline focus:no-underline">
           <span class="align-middle text-secondary font-bold text-2xl"
-            >DEMOAPP</span
+            >get it!</span
           >
         </nuxt-link>
       </div>
